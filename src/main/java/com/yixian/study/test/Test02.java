9a9d678a43584d1e70d842f518cc80e428a3369c
dongjiarui <dongjiarui@qq.com>
2023-03-09 20:32:24 +0800

    555

--- src/main/java/com/yixian/study/test/Test02.java
+++ src/main/java/com/yixian/study/test/Test02.java
@@ -8,10 +8,12 @@ package com.yixian.study.test;
  * @date 2023-02-13 14:59
  */
 public class Test02 {
-
-    private String test;
-
     public static void main(String[] args) {
+        System.out.println("重生之我叫王迪");
+
+        //迪哥专属
         System.out.println("我是王迪");
     }
+    private String lllll;
+
 }
